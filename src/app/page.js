4fca8556ad17f16efbb4042f90de5b872692b15b
Evelyn Oliveira <evelynoliveira@MacBook-Air-de-Evelyn.local>
2025-11-28 11:12:30 -0300

    Ajustes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+    <Link href={'/sobre'}> Acessar a pagina sobre  </Link>
     </div>
   );
 }
---
+++ src/app/sobre/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Sobre() {
+  return (
+    <div>
+      <h1>Página Sobre</h1>
+    <Link href={'/'}> Voltar para página inicial  </Link>
+    </div>
+  );
+}
