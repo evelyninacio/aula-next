95f8dc9095486a3a44f3cd953e53f1b9b788bcb7
Evelyn Oliveira <evelynoliveira@MacBook-Air-de-Evelyn.local>
2025-12-04 23:30:31 -0300

    adicionando api na home

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,27 @@
 import Link from "next/link";
 
-export default function Home() {
+
+// Este componente é assíncrono e roda no servidor por padrão no App Router.
+// O Next.js vai pré-renderizar esta página em tempo de build (SSG),
+// desde que o fetch seja estático (sem coisas baseadas em cookies, headers dinâmicos, etc.)
+export default async function Home() {
+  const res = await fetch(
+    "https://worldtimeapi.org/api/timezone/America/Sao_Paulo"
+  );
+  const data = await res.json();
+
+  const horario = data.datetime;
+
   return (
     <div>
-      <h1>Hello World</h1>
-    <Link href={'/sobre'}> Acessar a pagina sobre  </Link>
+      <h1>Página SSG</h1>
+
+      <h3>Horário vindo de uma API externa no momento da build/pre-renderização:</h3>
+
+      {horario}
+      <br />
+
+      <Link href="/sobre">Ir para Sobre</Link>
     </div>
   );
 }
